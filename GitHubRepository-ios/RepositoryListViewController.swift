01c96d8107b1c6179d65c8b4ef7df652b66f9111
jinan <ja.choi@sweetk.co.kr>
2023-05-26 23:15:04 +0900

    1.RepositoryListViewController 코드수정 새로고침 기능 추가

--- GitHubRepository-ios/RepositoryListViewController.swift
+++ GitHubRepository-ios/RepositoryListViewController.swift
@@ -15,5 +15,37 @@ final class RepositoryListViewController: UITableViewController {
         super.viewDidLoad()
         
         title = organization + "Repositories"
+        
+        // 화면 아래로 당겨서 새로고침하는 기능추가
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        refreshControl.backgroundColor = .white
+        refreshControl.tintColor = .darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+        // refreshControl에 action 가능을 추가한다.
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        tableView.register(RepositoryListCell.self, forCellReuseIdentifier: "RepositoryListCell")
+        tableView.rowHeight = 140
     }
+    
+    // 네크워크 통신을 추가한다.
+    @objc func refresh() {
+        
+    }
+}
+
+// UITableView DataSource Delegate
+extension RepositoryListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryListCell", for: indexPath) as? RepositoryListCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
 }
