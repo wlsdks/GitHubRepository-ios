c086de7656bda7a664ea18590f55f5c6234abbb3
jinan <ja.choi@sweetk.co.kr>
2023-05-26 23:28:08 +0900

    1. Repository 데이터 구조체 생성 2. RepositoryListCell에서 cell설정 완료 : 레이아웃 추가

---
+++ GitHubRepository-ios/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GitHubRepository-ios
+//
+//  Created by 최진안 on 2023/05/26.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
